f72e6e190a2e3ccd7a451fe8f43bed7bc42dcf60
Rushikesh_Bhujbal <maharajrushi700@gmail.com>
2026-03-29 18:11:46 +0530

    Phase-2: Data cleaning and formatting implemented

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
-from utils import load_data
+from utils import load_data, clean_data
 
 df = load_data()
+df = clean_data(df)
+
 print(df)
+
+print("\nData Types:")
+print(df.dtypes)
--- utils.py
+++ utils.py
@@ -6,3 +6,12 @@ def load_data():
     file_path = os.path.join(base_path, "data", "finance.csv")
     df = pd.read_csv(file_path)
     return df
+
+def clean_data(df):
+    # Remove missing values
+    df.dropna(inplace=True)
+
+    # Convert date column to datetime
+    df['date'] = pd.to_datetime(df['date'])
+
+    return df
